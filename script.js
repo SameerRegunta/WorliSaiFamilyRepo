605de9bdf32b60067d258a853a55670cdc6b66e6
SameerRegunta <79760960+SameerRegunta@users.noreply.github.com>
2025-03-23 17:05:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const allEvents = [
     { name: "Dum Dum Dum Dum Damaru", date: "1 Jan 2023", year: "2023", month: "January", link: "https://sairhythms.sathyasai.org/song/dum-dum-dum-dum-damaru" },
     { name: "Jai Durga Lakshmi Saraswathi Sai Jaganmata", date: "5 Jan 2023", year: "2023", month: "January", link: "https://sairhythms.sathyasai.org/song/jai-durga-lakshmi-saraswathi-sai-jaganmata" },
-    { name: "Example Event 1", date: "10 Feb 2024", year: "2024", month: "February", link: "#" },
-    { name: "Example Event 2", date: "20 Mar 2024", year: "2024", month: "March", link: "#" },
-    {name:"Narayan Seva",date:"15 Feb 2025",year:"2025",month:"February",link:"https://photos.google.com/share/AF1QipOwHmkBr-JE_Uc9kJi5pazsT-paR0IHb9IzBO2kYl3cT0BATFw8gcqNo1GgqBINag?key=T1lMd3dubmU5RF9yNF9rTmpjNk95bFF0ZHlLMkp3"},
-    { name: "Ekadasha Rudrabhishekam", date: "23 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/5JqQ6PhmQ9jco3qu9"},
-    { name: "Gayatri Havan of Bhagwan’s Centenary Year Celebrations held at residence of Smt Indira Gone Garu", date: "22 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/jtH9Ng4vtfBimXEeA"},
-    { name: "Residential Bhajans of Bhagwan’s Centenary Year Celebrations held at Balvikas child – Sriyan Siripuram", date: "16 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/4vF6YofH6ZTvhFH78"},
+    { name: "Lambodhara Hey Vinayaka", date: "10 Feb 2024", year: "2024", month: "February", link: "https://sairhythms.sathyasai.org/song/lambodhara-hey-vinayaka" },
+    { name: "Shiva Shankara Parvathi Ramana", date: "20 Mar 2024", year: "2024", month: "March", link: "https://sairhythms.sathyasai.org/song/shiva-shankara-parvathi-ramana" },
+    { name: "Gayatri Havan of Bhagwan’s Centenary Year Celebrations held at the residence of Shri Pothu Sudarshan Garu", date: "13 Jan 2025", year: "2025", month: "January", link: "https://photos.app.goo.gl/AHZgPBvEhop9j79n7"},
     { name: "Narayan Seva", date: "18 Jan 2025", year: "2025", month: "January", link: "https://photos.app.goo.gl/1MbPTnCDHJCtcAGh6"},
-    { name: "Gayatri Havan of Bhagwan’s Centenary Year Celebrations held at the residence of Shri Pothu Sudarshan Garu", date: "13 Jan 2025", year: "2025", month: "January", link: "https://photos.app.goo.gl/AHZgPBvEhop9j79n7"}
+    { name: "Narayan Seva", date: "15 Feb 2025", year: "2025", month: "February", link: "https://photos.google.com/share/AF1QipOwHmkBr-JE_Uc9kJi5pazsT-paR0IHb9IzBO2kYl3cT0BATFw8gcqNo1GgqBINag?key=T1lMd3dubmU5RF9yNF9rTmpjNk95bFF0ZHlLMkp3"},
+    { name: "Ekadasha Rudrabhishekam", date: "23 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/5JqQ6PhmQ9jco3qu9"},
+    { name: "Gayatri Havan of Bhagwan’s Centenary Year Celebrations held at the residence of Smt Indira Gone Garu", date: "22 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/jtH9Ng4vtfBimXEeA"},
+    { name: "Residential Bhajans of Bhagwan’s Centenary Year Celebrations held at Balvikas child – Sriyan Siripuram", date: "16 Feb 2025", year: "2025", month: "February", link: "https://photos.app.goo.gl/4vF6YofH6ZTvhFH78"},
 ];
 
 function showEvents(year) {
